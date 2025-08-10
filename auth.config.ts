da16685bd256daf97c3506575d63c20116940613
Ririn Yulandari <ririnyulandarii@gmail.com>
2025-08-10 21:31:13 +0800

    Memperbaiki provider credentials

--- auth.config.ts
+++ auth.config.ts
@@ -1,5 +1,45 @@
 import type { NextAuthConfig } from 'next-auth';
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import bcrypt from 'bcrypt';
+import credentials from 'next-auth/providers/credentials'; // Tambahkan baris ini!
+ 
+// Simulasi mendapatkan pengguna dari database
+async function getUser(email: string) {
+  try {
+    const user = await sql`SELECT * FROM users WHERE email=${email}`;
+    return user.rows[0];
+  } catch (error) {
+    console.error('Failed to fetch user:', error);
+    throw new Error('Failed to fetch user.');
+  }
+}
  
 export const authConfig = {
-  providers: [], // Tambahkan provider di sini jika dibutuhkan
+  // Array providers untuk mendefinisikan metode login
+  providers: [
+    // Provider `credentials` untuk autentikasi email dan sandi
+    credentials({
+      async authorize(credentials) {
+        // Validasi skema kredensial
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
+ 
+        if (parsedCredentials.success) {
+          const { email, password } = parsedCredentials.data;
+          const user = await getUser(email);
+          if (!user) return null;
+ 
+          // Membandingkan sandi
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+ 
+          if (passwordsMatch) return user;
+        }
+ 
+        console.log('Invalid credentials');
+        return null;
+      },
+    }),
+  ],
 } satisfies NextAuthConfig;
